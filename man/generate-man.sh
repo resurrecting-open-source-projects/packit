35fae9965eb3182e1664a6e37678c7c6cf4a16eb
Joao Eriberto Mota Filho <eriberto@debian.org>
2016-05-22 23:52:15 -0300

    Changed manpage to txt2man format

--- man/generate-man.sh
+++ man/generate-man.sh
@@ -10,6 +10,10 @@ P_VERSION=1.1
 P_MANLEVEL=8
 P_DESCRIPT="Packet analysis and injection tool"
 
-[ -e $P_NAME.txt ] || { echo "$P_NAME.txt not found"; exit 1; }
+TEST=$(txt2man -h 2> /dev/null)
+
+[ ! "$TEST" ] && { echo "ERROR: You need install txt2man program."; exit 1; }
+
+[ -e $P_NAME.txt ] || { echo "ERROR: $P_NAME.txt not found."; exit 1; }
 
 txt2man -d "$P_DATA" -t $P_NAME -r $P_NAME-$P_VERSION -s $P_MANLEVEL -v "$P_DESCRIPT" $P_NAME.txt > $P_NAME.$P_MANLEVEL
